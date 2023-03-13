bb0a922af901d9efb1e0c5511030bd514e2dce58
Worapong Sirilertpitak <markskytr@gmail.com>
2023-03-13 15:31:30 +0700

    goose

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program {
+   static void Main(string[] args)
+   {
+    int day,time;
+    string set;
+    int breakfast = 5; int weekend = 2; int coffee = 3;
+    Console.Write("Day :");
+    day = int.Parse(Console.ReadLine());
+    Console.Write("Time :");
+    time = int.Parse(Console.ReadLine());
+
+    while (true)
+    {
+       Console.Write("command :");
+       set = Console.ReadLine();
+
+       if (set!= "Breakfast set" && set!= "Weekend set" && set!= "Coffee" && set!= "End")
+       {
+        Console.WriteLine("What would you like to have?");
+        
+       }
+
+    }
+
+   }
+
+}
